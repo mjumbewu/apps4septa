bb5434791f9bf3f0f02568fd3a322e2ea156dd30
Aaron Ogle <atogle@gmail.com>
2011-10-08 17:23:13 -0700

    add detailed inset map

--- static/js/map.js
+++ static/js/map.js
@@ -1,10 +1,14 @@
 (function(){
-  var map = new L.Map('map');
+  var map = new L.Map('map'),
+    insetMap = new L.Map('inset-map', { zoomControl:false });
 
-  var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/22677/256/{z}/{x}/{y}.png',
-    cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-    cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttribution});
+  var cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
+    cloudmadeOutUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/45008/256/{z}/{x}/{y}.png',
+    cloudmadeOut = new L.TileLayer(cloudmadeOutUrl, {minZoom: 11, maxZoom: 14, attribution: cloudmadeAttribution}),
+    cloudmadeInUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/22677/256/{z}/{x}/{y}.png',
+    cloudmadeIn = new L.TileLayer(cloudmadeInUrl, {minZoom: 11, maxZoom: 14, attribution: false});
 
-  map.setView(new L.LatLng(39.952335,-75.163789), 12).addLayer(cloudmade);
+  map.setView(new L.LatLng(39.952335,-75.163789), 12).addLayer(cloudmadeOut);
+  insetMap.setView(new L.LatLng(39.952335,-75.163789), 12).addLayer(cloudmadeIn);
   
 })();
