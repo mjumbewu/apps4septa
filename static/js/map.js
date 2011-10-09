308954b0e3f8881239a3dd2daf20aa3e7e6979cf
Aaron Ogle <atogle@gmail.com>
2011-10-09 08:46:33 -0700

    update route details

--- static/js/map.js
+++ static/js/map.js
@@ -1,14 +1,14 @@
 (function(){
   var map = new L.Map('map'),
-    insetMap = new L.Map('inset-map', { zoomControl:false });
-
-  var cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
+    insetMap = new L.Map('inset-map', { zoomControl:false }),
+    cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
     cloudmadeOutUrl = 'http://{s}.tile.cloudmade.com/9f6894100760403498e3b47fdbdbcdef/45008/256/{z}/{x}/{y}.png',
     cloudmadeOut = new L.TileLayer(cloudmadeOutUrl, {minZoom: 15, maxZoom: 15, attribution: cloudmadeAttribution}),
     cloudmadeInUrl = 'http://{s}.tile.cloudmade.com/9f6894100760403498e3b47fdbdbcdef/46246/256/{z}/{x}/{y}.png',
     cloudmadeIn = new L.TileLayer(cloudmadeInUrl, {minZoom: 15, maxZoom: 15, attribution: false}),
     initCenter = new L.LatLng(39.952335,-75.163789),
-    initZoom = 15;
+    initZoom = 15,
+    $routeDetails = $('#route-details');
   
   var syncMap = function() {
     map.setView(insetMap.getCenter(), insetMap.getZoom());  
@@ -18,6 +18,19 @@
     insetMap.setView(map.getCenter(), map.getZoom());
   };
   
+  var updateRouteDetails = function(routes) {
+    var html = '';
+    if (routes && routes.length > 0) {
+      $.each(routes, function(i, r) {
+        html += '<h5><div class="swatch swatch-'+i+'"><div>Route '+r.label+'</h5>';
+      });
+    } else {
+      html = '<div class="alert-message block-message warning"><strong>No routes were found.</strong> No worries, just drag the map around and you\'ll find some.</div>';
+    }
+    
+    $routeDetails.html(html);
+  };
+  
   var updateRoutes = function(evt) {
     var bbox = insetMap.getBounds(),
       left = bbox.getSouthWest().lng,
@@ -25,9 +38,7 @@
       right = bbox.getNorthEast().lng,
       top = bbox.getNorthEast().lat,
       size = map.getSize();
-      
-    console.log(bbox, size);
-    
+          
     $.ajax({
       url: '/routes/' + left + ',' + bottom + ',' + right + ',' + top,
       data: {
@@ -37,9 +48,11 @@
       dataType: 'json',
       success: function(data, textStatus, jqXHR) {
         console.log(data, textStatus, jqXHR);
+        updateRouteDetails(data.routes);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.log(jqXHR, textStatus, errorThrown);
+        updateRouteDetails(false);
       }
     });
   };
@@ -56,4 +69,6 @@
   initMap(insetMap, cloudmadeIn, initCenter, initZoom, syncMap);
   
   insetMap.on('dragend', updateRoutes);
+  
+  updateRoutes();
 })();
